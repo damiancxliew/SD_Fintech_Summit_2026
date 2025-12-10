0824bcc0a309c028f9b30c1d6b4d3571afb97f25
JeromeG89 <jeromegoh89@gmail.com>
2025-12-10 17:40:00 +0800

    update sponsors

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -4,30 +4,18 @@ import SponsorSection from '../../components/sponsors/SponsorSection';
 const Sponsors = () => {
     const goldSponsors = [
         { name: "Blockchain for Good", imgSrc: "/sponsors/BlockchainGood.png", link: "https://www.blockchainforgood.xyz" },
-        { name: "Coinbase", imgSrc: "/sponsors/Coinbase.svg", link: "https://www.coinbase.com" },
-        { name: "EmbeddedLLM", imgSrc: "/sponsors/EmbeddedLLM.svg", link: "https://embeddedllm.com" },
+        { name: "GSR", imgSrc: "/sponsors/GSR.jpg", link: "https://www.gsr.io" },
         { name: "Flowdesk", imgSrc: "/sponsors/Flowdesk.png", link: "https://flowdesk.co" },
-        { name: "Jane Street", imgSrc: "/sponsors/Jane Street.png", link: "https://www.janestreet.com" },
-        { name: "Metacamp", imgSrc: "/sponsors/Metacamp.png", link: "https://metacamp.so" },
-        { name: "Nvidia", imgSrc: "/sponsors/Nvidia.png", link: "https://www.nvidia.com" }
     ];
     const platinumSponsors = [
-        { name: "Northern Trust", imgSrc: "/sponsors/Northern Trust.png", link: "https://www.northerntrust.com" },
         { name: "Ripple", imgSrc: "/sponsors/Ripple.png", link: "https://ripple.com" }
     ];
-    const silverSponsors = [
-        { name: "Bank of America", imgSrc: "/sponsors/Bank of America.png", link: "https://www.bankofamerica.com" },
-        { name: "Coingecko", imgSrc: "/sponsors/CoinGecko.png", link: "https://www.coingecko.com" },
-        { name: "Dethings", imgSrc: "/sponsors/DeThings.png", link: "https://dethings.com" },
-        { name: "eFinancialCareers", imgSrc: "/sponsors/eFinancialCareers.png", link: "https://www.efinancialcareers.sg" },
-        { name: "Taisu Ventures", imgSrc: "/sponsors/Taisu.png", link: "https://www.taisu.io" },
-
-    ];
+    // const silverSponsors = [];
     const communityPartners = [
-        { name: "Builderdao", imgSrc: "/sponsors/Builderdao.png", link: "https://buidlerdao.xyz/" },
-        { name: "Gigdao", imgSrc: "/sponsors/Gigdao.png", link: "https://gigdao.io" },
-        { name: "Moledao", imgSrc: "/sponsors/Moledao.png", link: "https://moledao.io" },
-        { name: "Seegrowth", imgSrc: "/sponsors/Seegrowth.png", link: "https://seegrowth.framer.website/" },
+        // { name: "Builderdao", imgSrc: "/sponsors/Builderdao.png", link: "https://buidlerdao.xyz/" },
+        // { name: "Gigdao", imgSrc: "/sponsors/Gigdao.png", link: "https://gigdao.io" },
+        // { name: "Moledao", imgSrc: "/sponsors/Moledao.png", link: "https://moledao.io" },
+        // { name: "Seegrowth", imgSrc: "/sponsors/Seegrowth.png", link: "https://seegrowth.framer.website/" },
     ];
 
     return (
@@ -44,12 +32,12 @@ const Sponsors = () => {
                 <div className="flex-grow order-2 lg:order-2">
                     <SponsorSection title="Gold" sponsors={goldSponsors} />
                 </div>
-                <div className="flex-grow order-3 lg:order-3">
+                {/* <div className="flex-grow order-3 lg:order-3">
                     <SponsorSection title="Silver" sponsors={silverSponsors} />
-                </div>
-                <div className="flex-grow order-4 lg:order-4">
+                </div> */}
+                {/* <div className="flex-grow order-4 lg:order-4">
                     <SponsorSection title="Community Partners" sponsors={communityPartners} />
-                </div>
+                </div> */}
             </div>
         </div>
     );
