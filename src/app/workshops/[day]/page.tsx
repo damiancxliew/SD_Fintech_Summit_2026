5c95289621e5c4c066b746ed54d455655e71d468
JeromeG89 <jeromegoh89@gmail.com>
2025-12-10 13:43:43 +0800

    workshop tbc

--- src/app/workshops/[day]/page.tsx
+++ src/app/workshops/[day]/page.tsx
@@ -10,22 +10,29 @@ export default async function Page({ params }: { params: { day: string } }) {
   const workshops = await getWorkshopsAsync(day);
   const panels = panelsMockData[day] || []; // Get panels for the day, default to empty array if no panels exist
 
-  return (
-    <div className="bg-custom-background min-h-screen p-8">
-      {/* DAY label at the top */}
-      <h1 className="text-2xl text-center text-white font-bold mb-8">
-        <div className="[text-shadow:_4px_3px_0px_#000000] text-5xl">
-          {getDayString(day)} {/* Show the dynamic day title */}
-        </div>
-      </h1>
+  // return (
+  //   <div className="bg-custom-background min-h-screen p-8">
+  //     {/* DAY label at the top */}
+  //     <h1 className="text-2xl text-center text-white font-bold mb-8">
+  //       <div className="[text-shadow:_4px_3px_0px_#000000] text-5xl">
+  //         {getDayString(day)} {/* Show the dynamic day title */}
+  //       </div>
+  //     </h1>
 
-      {/* Workshops Section */}
-      <h2 className="text-xl text-center text-white font-semibold mb-4">
-        <div className="[text-shadow:_4px_3px_0px_#000000] text-3xl">
-          {"PHYSICAL WORKSHOPS"} {/* Show the dynamic workshop type */}
-        </div>
-      </h2>
-      <WorkshopList workshops={workshops} />
+  //     {/* Workshops Section */}
+  //     <h2 className="text-xl text-center text-white font-semibold mb-4">
+  //       <div className="[text-shadow:_4px_3px_0px_#000000] text-3xl">
+  //         {"PHYSICAL WORKSHOPS"} {/* Show the dynamic workshop type */}
+  //       </div>
+  //     </h2>
+  //     <WorkshopList workshops={workshops} />
+  //   </div>
+  // );
+  return ( //TODO: change back when workshop data is set
+    <div className="bg-custom-background min-h-screen p-8 flex items-center justify-center">
+      <h1 className="text-4xl text-white font-bold [text-shadow:_4px_3px_0px_#000000]">
+        To be released
+      </h1>
     </div>
   );
 }
