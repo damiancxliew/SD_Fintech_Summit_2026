6a2d05f1e9bf2b1043b86ac2b5cb79c7767e2821
Damian Liew Cho Xiang <damiancxliew@gmail.com>
2026-01-02 10:43:18 +0800

    update panel date

--- src/components/panels/panelsData.ts
+++ src/components/panels/panelsData.ts
@@ -4,7 +4,7 @@ import { IPanels } from './types'; // Make sure the path to types.ts is correct
 export const panelsMockData: Record<string, IPanels[]> = {
   day1: [
     {
-      date: "2026-01-09",
+      date: "9 Jan 2026",
       time: "11:00AM - 11:30AM",
       venue: "NUS School of Computing (COM3)",
       topic: "Stablecoins as Global Commerce's New Engine",  // The new topic field
